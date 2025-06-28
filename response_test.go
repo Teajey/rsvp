1848d453b2dbaab108d15a6f92137dc1062d8097
Teajey <21069848+Teajey@users.noreply.github.com>
2025-06-29 08:29:02 +0900

    Test json response

--- response_test.go
+++ response_test.go
@@ -25,3 +25,36 @@ World!`
 		t.Fatalf("s does not have the expected value: %#v", s)
 	}
 }
+
+func TestStringBodyAcceptApp(t *testing.T) {
+	body := `Hello,
+World!`
+	res := rsvp.Response{Body: body}
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("Accept", "application/json")
+	rec := httptest.NewRecorder()
+
+	err := res.Write(rec, req, nil, nil)
+	assert.FatalErr(t, "Write response", err)
+
+	statusCode := rec.Result().StatusCode
+	assert.Eq(t, "Status code", 200, statusCode)
+
+	s := rec.Body.String()
+	assert.Eq(t, "body contents", `["hello","world","123"]`+"\n", s)
+}
+
+func TestListBody(t *testing.T) {
+	body := []string{"hello", "world", "123"}
+	res := rsvp.Response{Body: body}
+	req := httptest.NewRequest("GET", "/", nil)
+	rec := httptest.NewRecorder()
+
+	err := res.Write(rec, req, nil, nil)
+	assert.FatalErr(t, "Write response", err)
+
+	statusCode := rec.Result().StatusCode
+	assert.Eq(t, "Status code", 200, statusCode)
+	s := rec.Body.String()
+	assert.Eq(t, "body contents", `["hello","world","123"]`+"\n", s)
+}
