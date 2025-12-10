1bac5af9cb75112a55f10c1899c1058231fc0cb1
Teajey <21069848+Teajey@users.noreply.github.com>
2025-12-11 08:28:44 +0900

    Replace MiddleHandleFunc with better middleware

--- handler.go
+++ handler.go
@@ -28,30 +28,56 @@ type Handler interface {
 
 type HandlerFunc func(h http.Header, r *http.Request) Response
 
-// Uses the same pattern syntax as http.ServeMux
-func (m *ServeMux) HandleFunc(pattern string, handler HandlerFunc) {
-	m.MiddleHandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) Response {
-		response := handler(w.Header(), r)
-
-		return response
-	})
-}
-
-// Register an RSVP handler with access to http.ResponseWriter
-//
-// The intention is to make adapting net/http middleware to RSVP easier
-func (m *ServeMux) MiddleHandleFunc(pattern string, handler func(h http.ResponseWriter, r *http.Request) Response) {
-	m.Std.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
-		response := handler(w, r)
-
-		err := response.Write(w, r, m.Config)
-		if err != nil {
-			panic(fmt.Sprintf("Failed to write rsvp.Response: %s", err))
-		}
-	})
+func (f HandlerFunc) ServeHTTP(h http.Header, r *http.Request) Response {
+	return f(h, r)
 }
 
 // Uses the same pattern syntax as http.ServeMux
 func (m *ServeMux) Handle(pattern string, handler Handler) {
-	m.HandleFunc(pattern, handler.ServeHTTP)
+	m.Std.Handle(pattern, Middleware(m.Config, handler))
+}
+
+// Uses the same pattern syntax as http.ServeMux
+func (m *ServeMux) HandleFunc(pattern string, handler HandlerFunc) {
+	m.Handle(pattern, handler)
+}
+
+// An rsvp.Handler with access to the writer. Only intended as an
+// adapter for middleware that takes http.ResponseWriter. Care should
+// be taken that the body is not written to, as that must be handled by
+// func (*Response) Write
+type MiddleHandler interface {
+	ServeHTTP(w http.ResponseWriter, r *http.Request) Response
+}
+
+type MiddleHandlerFunc func(w http.ResponseWriter, r *http.Request) Response
+
+func (f MiddleHandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) Response {
+	return f(w, r)
+}
+
+// Can be used to run a MiddleHandler, It will run before rsvp writes to the
+// HTTP response body
+func MiddlewareBeforeWriteResponse(cfg *Config, next MiddleHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response := next.ServeHTTP(w, r)
+		err := response.Write(w, r, cfg)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to write rsvp.Response: %s", err))
+		}
+	}
+}
+
+func middlewareGetArgs(next Handler) MiddleHandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) Response {
+		headers := w.Header()
+		return next.ServeHTTP(headers, r)
+	}
+}
+
+// Middleware adapter for individual handlers. Is useful for
+// adding rsvp to handlers selectively, or plugging in rsvp
+// without the provided ServeMux
+func Middleware(cfg *Config, next Handler) http.HandlerFunc {
+	return MiddlewareBeforeWriteResponse(cfg, middlewareGetArgs(next))
 }
