b61c48e4850300f31da9d0625046a204777ea176
Teajey <21069848+Teajey@users.noreply.github.com>
2026-01-27 21:50:35 +0900

    Simplify middleware

--- middleware.go
+++ middleware.go
@@ -7,63 +7,31 @@ import (
 	"github.com/Teajey/rsvp/internal/log"
 )
 
-// WriterHandler is a [Handler] with access to [http.ResponseWriter].
+// AdaptHandlerFunc provides rsvp as middleware.
 //
-// This interface is primarily intended as an adapter for third-party middleware
-// that requires [http.ResponseWriter].
-//
-// WARNING: Care should be taken that [http.ResponseWriter.WriteHeader] is not used here, as response
-// status is managed by [Response.Write]. Calling WriteHeader will cause incorrect
-// behavior.
-type WriterHandler interface {
-	ServeHTTP(w http.ResponseWriter, r *http.Request) Response
-}
-
-type WriterHandlerFunc func(w http.ResponseWriter, r *http.Request) Response
-
-func (f WriterHandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) Response {
-	return f(w, r)
-}
-
-// Adapt wraps [WriterHandler] and returns the standard [http.HandlerFunc].
-func Adapt(cfg Config, handler WriterHandler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		response := handler.ServeHTTP(w, r)
-		if err := response.Write(w, r, cfg); err != nil {
-			http.Error(w, "RSVP failed to write a response", http.StatusInternalServerError)
+// It may be used to add rsvp to discrete handlers.
+// It is also useful for wrapping rsvp handlers in middleware that requires access to [http.ResponseWriter].
+func AdaptHandlerFunc(cfg Config, next HandlerFunc) http.HandlerFunc {
+	return func(wr http.ResponseWriter, r *http.Request) {
+		w := response{
+			std: wr,
+		}
+		response := next(&w, r)
+		if response.TemplateName == "" && w.defaultTemplateName != "" {
+			log.Dev("Using default template name: %v", w.defaultTemplateName)
+			response.TemplateName = w.defaultTemplateName
+		}
+		if err := response.Write(wr, r, cfg); err != nil {
+			http.Error(wr, "RSVP failed to write a response", http.StatusInternalServerError)
 			stdlog.Printf("[ERROR] RSVP failed to write a response: %s", err)
 		}
 	}
 }
 
-// AdaptFunc is a convenience that wraps [Adapt] for [WriterHandlerFunc]
-func AdaptFunc(cfg Config, handler func(w http.ResponseWriter, r *http.Request) Response) http.HandlerFunc {
-	return Adapt(cfg, WriterHandlerFunc(handler))
-}
-
-// toWriterHandler adapts a [Handler] to [WriterHandler]
-func toWriterHandler(h Handler) WriterHandler {
-	return WriterHandlerFunc(func(wr http.ResponseWriter, r *http.Request) Response {
-		w := response{
-			std: wr,
-		}
-		res := h.ServeHTTP(&w, r)
-		if res.TemplateName == "" && w.defaultTemplateName != "" {
-			log.Dev("Using default template name: %v", w.defaultTemplateName)
-			res.TemplateName = w.defaultTemplateName
-		}
-		return res
-	})
-}
-
-// AdaptHandler wraps a [Handler] and returns a standard [http.HandlerFunc].
-// Convenience for integrating with net/http.
-func AdaptHandler(cfg Config, h Handler) http.HandlerFunc {
-	return Adapt(cfg, toWriterHandler(h))
-}
-
-// AdaptHandlerFunc wraps a [HandlerFunc] and returns a standard [http.HandlerFunc].
-// Convenience for integrating with net/http.
-func AdaptHandlerFunc(cfg Config, h func(w ResponseWriter, r *http.Request) Response) http.HandlerFunc {
-	return Adapt(cfg, toWriterHandler(HandlerFunc(h)))
+// AdaptHandler provides rsvp as middleware.
+//
+// It may be used to add rsvp to discrete handlers.
+// It is also useful for wrapping rsvp handlers in middleware that requires access to [http.ResponseWriter].
+func AdaptHandler(cfg Config, next Handler) http.Handler {
+	return AdaptHandlerFunc(cfg, next.ServeHTTP)
 }
--- middleware_test.go
+++ middleware_test.go
@@ -15,25 +15,36 @@ type T struct {
 	t *testing.T
 }
 
-func (t T) handler(s cookieSetter) rsvp.Response {
+func (t T) handler(w rsvp.ResponseWriter, r *http.Request, s cookieSetter) rsvp.Response {
 	cookies, err := http.ParseCookie("hello=world")
 	assert.FatalErr(t.t, "cookie parse", err)
 	s(cookies[0])
 	return rsvp.Blank()
 }
 
-func TestMiddlewareBeforeWriteResponse(t *testing.T) {
-	cfg := rsvp.Config{}
-	server := httptest.NewServer(rsvp.Adapt(cfg, rsvp.WriterHandlerFunc(func(w http.ResponseWriter, r *http.Request) rsvp.Response {
+func middleware(next func(w rsvp.ResponseWriter, r *http.Request, s cookieSetter) rsvp.Response) func(w http.ResponseWriter, r *http.Request) rsvp.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) rsvp.HandlerFunc {
 		setter := func(cookie *http.Cookie) {
 			http.SetCookie(w, cookie)
 		}
-		return T{t}.handler(setter)
-	})))
+		return func(w rsvp.ResponseWriter, r *http.Request) rsvp.Response {
+			return next(w, r, setter)
+		}
+	}
+}
+
+func TestMiddlewareBeforeWriteResponse(t *testing.T) {
+	cfg := rsvp.Config{}
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		a := middleware(T{t}.handler)
+		b := a(w, r)
+		rsvp.AdaptHandlerFunc(cfg, b)(w, r)
+	}))
 	defer server.Close()
 
 	res, err := http.Get(server.URL)
 	assert.FatalErr(t, "server response", err)
 
+	assert.Eq(t, "status code", res.StatusCode, 200)
 	assert.Eq(t, "expected cookie header", "hello=world", res.Header.Get("Set-Cookie"))
 }
